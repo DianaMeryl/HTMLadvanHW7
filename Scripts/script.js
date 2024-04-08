71921123fbb7c742b97c4c102803d242c2d258ab
DianaMeryl <119227850+DianaMeryl@users.noreply.github.com>
2024-04-08 23:57:41 +0300

    first commit

---
+++ Scripts/script.js
@@ -0,0 +1,16 @@
+const ways = [
+    {
+        title: "Hello",
+        description: "from UA",
+    },
+    {
+        title: "Hi",
+        description: "From London",
+    },
+    {
+        title:"bonjue",
+        description: "from Paris"
+    },
+]
+
+console.log({...ways[1]});
